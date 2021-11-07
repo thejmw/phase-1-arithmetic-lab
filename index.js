41453ba87245a0640cd9a1631ef0d417e4e395c5
Jason <thejasonmichaelwilliams@gmail.com>
2021-11-07 18:32:45 -0500

    commit tree 1ddf89573d09f540693030ca1729d8c20b31a7a1

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+const chai = "chai"
+
+let num1 = 2
+let num2 = 31
+var multiply = num1 * num2
+
+let random = Math.floor(Math.random() *10) +1;
+console.log(random)
+
+let num3 = 12
+let num4 = 3
+var mod = num3 / num4
+
+let max = Math.max(20)
